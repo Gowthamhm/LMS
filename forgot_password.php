38f7bd21f4884a55acf4263956f4582fafa12467
Gowthamhm <gowthamhm001@gmail.com>
2021-03-06 21:32:46 +0530

    login and forgot password html part

---
+++ forgot_password.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Forgot Password</title>
+  </head>
+  <body>
+
+
+  <div class="d-md-flex half">
+    <div class="bg" style="background-image: url('images/bg.jpg');"></div>
+    <div class="contents">
+
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-12">
+            <div class="form-block mx-auto">
+              <div class="text-center mb-5">
+                <h3 class="text-uppercase">Reset Password</h3>
+              </div>
+              <form action="#" method="post">
+                <div class="form-group first">
+                  <label for="Email">Email</label>
+                  <input type="email" class="form-control" placeholder="abc@gmail.com" id="username">
+                </div>
+                <input type="submit" value="Reset Password" class="btn btn-block py-2 btn-primary">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
